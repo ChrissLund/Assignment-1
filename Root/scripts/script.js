d56d6dd86756b9fbfe34a6482dde1ad5fcd92a09
ChrissLund <christinelundgren@gmail.com>
2024-03-19 12:00:40 +0100

    Assignment 1

---
+++ Root/scripts/script.js
@@ -0,0 +1,13 @@
+
+function onButtonClick(){
+    alert ('Divide');
+    }
+
+var button = document.querySelector('button')
+button.addEventListener('click', onButtonClick);
+
+let foo
+foo = 0
+let bar
+bar = 0
+bar/foo
